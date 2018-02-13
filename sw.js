c6382bd29f3ee4fed3e06e15f108ef30b865aa1f
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-02-13 15:02:30 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,23 @@
-let filesToCache = [
-  '/styles.css',
-  'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
-  'data/restaurants.json',
-  'index.html',
-  'restaurant.html',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/js/dbhelper.js'
-];
-
-let staticCacheName = 'pages-cache-v1';
+let cacheName = 'offlinerocks';
 
 self.addEventListener('install', function(event) {
   console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open(staticCacheName)
-    .then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
+      caches.open(cacheName).then function(cache) {
+        return cache.addAll([
+            '/css/styles.css',
+            'index.html',
+            'restaurant.html'
+        ]);
+      })
+    )
+})
+
+self.addEventListener("fetch", function(event) {
+  console.log("fetching..")
+  event.respondWith(
+    caches.open(cacheName).then(function(cache)) {
+      return cache.match(event.request);
+    }
   );
 });
