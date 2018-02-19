725b3df801549e9f6ef9404cee31252d37038609
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-02-18 23:12:56 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-let cacheName = 'offlinerocks';
-
-self.addEventListener('install', function(event) {
-  console.log('Attempting to install service worker and cache static assets');
+self.addEventListener('install', function (event) {
   event.waitUntil(
-      caches.open(cacheName).then function(cache) {
-        return cache.addAll([
-            '/css/styles.css',
-            'index.html',
-            'restaurant.html'
-        ]);
-      })
-    )
-})
+    caches.open('restaurant-static').then(function (cache) {
+      return cache.addAll([
+        '/css/styles.css',
+        'index.html',
+        'restaurant.html',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+      ]);
+    }));
+});
 
-self.addEventListener("fetch", function(event) {
-  console.log("fetching..")
-  event.respondWith(
-    caches.open(cacheName).then(function(cache)) {
-      return cache.match(event.request);
+
+self.addEventListener('fetch', function (event) {
+  event.RespondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
     }
-  );
+));
 });
