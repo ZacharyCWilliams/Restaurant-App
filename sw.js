10a8c90ba764541a27e439d3bfad2a7ecda35e90
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-02-21 12:28:11 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ self.addEventListener('install', function (event) {
 
 
 self.addEventListener('fetch', function (event) {
-  event.RespondWith(
+  event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) return response;
       return fetch(event.request);
